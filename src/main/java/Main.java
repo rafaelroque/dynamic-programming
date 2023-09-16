33ce555599a63cb955acd46b9a601a0e1fbf3e03
Rafael Roque Viana <rafa834@gmail.com>
2023-09-16 13:17:04 -0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String main = """
+                This
+                is
+                a
+                test
+                """;
+
+        System.out.println(main);
+    }
+}
